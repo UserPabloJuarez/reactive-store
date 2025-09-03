ce45d08d22d64d88501808374b50b605f5a78c93
unknown <pajhim@hotmail.com>
2025-09-03 17:42:23 -0500

    Creating the repository in Git

---
+++ src/test/java/com/interbank/reactive_store/ReactiveStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interbank.reactive_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
